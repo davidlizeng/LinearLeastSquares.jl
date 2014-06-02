fd196eb9a5bdab0a707397e8bd67117cd0d1ec55
David Zeng <davidlzeng@gmail.com>
2014-06-02 03:44:33 -0700

    getindex cannot call multplication, due to vectorization changing size.

--- src/atoms/index.jl
+++ src/atoms/index.jl
@@ -1,3 +1,4 @@
+import Base.getindex
 export getindex
 
 function getindex{T <: Real}(x::AffineExpr, rows::AbstractArray{T, 1}, cols::AbstractArray{T, 1} = [1])
@@ -5,23 +6,31 @@ function getindex{T <: Real}(x::AffineExpr, rows::AbstractArray{T, 1}, cols::Abs
   num_rows_coeff = length(rows) * length(cols)
   num_cols_coeff = x.size[1] * x.size[2]
 
-  coeff = spzeros(num_rows_coeff, num_cols_coeff)
+  indexer_val = spzeros(num_rows_coeff, num_cols_coeff)
 
-  # Create the coeff matrix such that coeff * vec(x) = vec(x[rows, cols])
+  # Create the indexing matrix such that indexer * vec(x) = vec(x[rows, cols])
   k = 1
   num_rows = x.size[1]
   for c in cols
     for r in rows
-      idx = num_rows * (c - 1) + r
-      coeff[k, idx] = 1
+      idx = num_rows * (convert(Int64, c) - 1) + convert(Int64, r)
+      indexer_val[k, idx] = 1
       k += 1
     end
   end
-  
-  return coeff * x
+
+  indexer = Constant(indexer_val)
+
+  vars_to_coeffs_map = Dict{Uint64, Constant}()
+  for (v, c) in x.vars_to_coeffs_map
+    vars_to_coeffs_map[v] = indexer * c
+  end
+  constant = indexer * x.constant
+  this = AffineExpr(:getindex, vars_to_coeffs_map, constant, x.sign, (length(rows), length(cols)))
+  #TODO eval
+  return this
 end
 
-getindex{T <: Real}(x::AffineExpr, rows::AbstractArray{1, T}) = getindex(x, rows')
 getindex(x::AffineExpr, row::Number, col::Number = 1) = getindex(x, row:row, col:col)
 getindex{T <: Real}(x::AffineExpr, row::Number, cols::AbstractArray{T, 1}) = getindex(x, row:row, cols)
 getindex{T <: Real}(x::AffineExpr, rows::AbstractArray{T, 1}, col::Number) = getindex(x, rows, col:col)
